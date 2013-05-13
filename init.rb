8a4cce60cf9778f2081914ad0866b7ff5f1c7ea7
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-05-13 09:48:50 +0900

    RAILS_DEFAULT_LOGGER is duprecated.

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 # Redmine RD formatter
 require 'redmine'
 
-RAILS_DEFAULT_LOGGER.info 'Starting RD formatter for RedMine'
+Rails.logger.info 'Starting RD formatter for RedMine'
 
 Redmine::Plugin.register :redmine_rd_formatter do
   name 'RD formatter'
